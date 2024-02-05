b2540f7072978a1d07ca50cfbc11299ca0bc0b7a
parkjjoe <105961163+parkjjoe@users.noreply.github.com>
2024-02-05 13:35:09 +0900

    [Bronze I] Title: 세로읽기, Time: 44 ms, Memory: 31120 KB -BaekjoonHub

---
+++ 백준/Bronze/10798. 세로읽기/세로읽기.py
@@ -0,0 +1,8 @@
+import sys
+
+word = [sys.stdin.readline().rstrip() for _ in range(5)]
+
+for i in range(15): # 한 행에 최대 15개
+    for j in range(5): # 총 5열
+        if i < len(word[j]): # 어떤 행의 i번째 문자가 그 행의 길이보다 작으면 출력
+            print(word[j][i], end="")
