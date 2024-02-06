f7684e1d1c51d8bb6e87918c93900bdc8a905b35
parkjjoe <105961163+parkjjoe@users.noreply.github.com>
2024-02-06 14:07:12 +0900

    [Bronze II] Title: 진법 변환, Time: 40 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Bronze/2745. 진법 변환/진법 변환.py
+++ 백준/Bronze/2745. 진법 변환/진법 변환.py
@@ -3,3 +3,15 @@ import sys
 n, b = map(str, sys.stdin.readline().rstrip().split())
 
 print(int(n, int(b)))
+
+# 더 복잡한 코드
+# 
+# n, b = map(str, sys.stdin.readline().rstrip().split())
+# array = "0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+# n = n[::-1] # 진법 계산을 위한 역순 처리
+# answer = 0
+# 
+# for i in range(len(n)):
+#     answer += (i ** int(b)) * (array.index(n[i])) # ex. 0^36 * Z + 1^36 * Z + ...
+# 
+# print(answer)
