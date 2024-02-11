6d1762cfd379801fb2d072ed1315b6033de4cd85
parkjjoe <105961163+parkjjoe@users.noreply.github.com>
2024-02-11 22:35:43 +0900

    [Bronze V] Title: 알고리즘 수업 - 알고리즘의 수행 시간 1, Time: 40 ms, Memory: 31120 KB -BaekjoonHub

---
+++ 백준/Bronze/24262. 알고리즘 수업 － 알고리즘의 수행 시간 1/알고리즘 수업 － 알고리즘의 수행 시간 1.py
@@ -0,0 +1,4 @@
+print(1)
+print(0)
+
+# https://kevinitcoding.tistory.com/entry/%EB%B0%B1%EC%A4%80Python-24262%EB%B2%88-%EC%95%8C%EA%B3%A0%EB%A6%AC%EC%A6%98-%EC%88%98%EC%97%85-%EC%95%8C%EA%B3%A0%EB%A6%AC%EC%A6%98%EC%9D%98-%EC%88%98%ED%96%89-%EC%8B%9C%EA%B0%84-1-%EB%AC%B8%EC%A0%9C
