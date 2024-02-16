1ac05ce3420c0775708ebb86f90ff1ed7bdc0db0
parkjjoe <105961163+parkjjoe@users.noreply.github.com>
2024-02-16 12:10:20 +0900

    [Silver III] Title: 소수 구하기, Time: 5260 ms, Memory: 33240 KB -BaekjoonHub

---
+++ 백준/Silver III/1929. 소수 구하기/소수 구하기.py
@@ -0,0 +1,14 @@
+import sys
+import math
+
+m, n = map(int, sys.stdin.readline().split())
+
+for i in range(m, n+1):
+    if i == 1:
+        continue
+
+    for j in range(2, int(math.sqrt(i)) + 1):
+        if i % j == 0:
+            break
+    else:
+        print(i)
