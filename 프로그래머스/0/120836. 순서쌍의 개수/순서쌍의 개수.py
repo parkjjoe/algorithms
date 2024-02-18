35566598b60253bbde94abb906e05002d2c9a5b4
parkjjoe <105961163+parkjjoe@users.noreply.github.com>
2024-02-18 16:28:42 +0900

    [level 0] Title: 순서쌍의 개수, Time: 63.65 ms, Memory: 10.2 MB -BaekjoonHub

---
+++ 프로그래머스/0/120836. 순서쌍의 개수/순서쌍의 개수.py
@@ -0,0 +1,10 @@
+from itertools import combinations
+
+def solution(n):
+    num = []
+    
+    for i in range(1, n+1):
+        if n % i == 0:
+            num.append(i)
+    
+    return len(num)
