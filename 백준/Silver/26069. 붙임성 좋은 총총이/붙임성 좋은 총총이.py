4de60acac8a4841b985364636ba3213b37a0bbd3
parkjjoe <105961163+parkjjoe@users.noreply.github.com>
2024-03-01 13:45:20 +0900

    [Silver IV] Title: 붙임성 좋은 총총이, Time: 40 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Silver/26069. 붙임성 좋은 총총이/붙임성 좋은 총총이.py
+++ 백준/Silver/26069. 붙임성 좋은 총총이/붙임성 좋은 총총이.py
@@ -7,11 +7,7 @@ dance = set()
 for _ in range(n):
     a, b = map(str, sys.stdin.readline().rstrip().split())
 
-    if "ChongChong" == a or "ChongChong" == b:
-        dance.add(a)
-        dance.add(b)
-
-    if a in dance or b in dance:
+    if "ChongChong" == a or "ChongChong" == b or a in dance or b in dance:
         dance.add(a)
         dance.add(b)
 
