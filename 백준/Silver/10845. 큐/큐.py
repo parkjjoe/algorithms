0281a6daf399b2147e45f19c25ad8ced74eb5ae0
parkjjoe <105961163+parkjjoe@users.noreply.github.com>
2024-03-03 14:57:56 +0900

    [Silver IV] Title: 큐, Time: 72 ms, Memory: 34036 KB -BaekjoonHub

---
+++ 백준/Silver/10845. 큐/큐.py
@@ -0,0 +1,22 @@
+import sys
+from collections import deque
+
+n = int(sys.stdin.readline())
+
+queue = deque()
+
+for _ in range(n):
+    order = list(map(str, sys.stdin.readline().rstrip().split()))
+
+    if order[0] == "push":
+        queue.append(order[1])
+    elif order[0] == "pop":
+        print(-1) if not queue else print(queue.popleft())
+    elif order[0] == "size":
+        print(len(queue))
+    elif order[0] == "empty":
+        print(1) if not queue else print(0)
+    elif order[0] == "front":
+        print(-1) if not queue else print(queue[0])
+    elif order[0] == "back":
+        print(-1) if not queue else print(queue[-1])
