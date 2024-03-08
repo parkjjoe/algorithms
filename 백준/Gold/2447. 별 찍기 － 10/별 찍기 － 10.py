156e1081c6c655cd75aaf506e63d899bc8229973
parkjjoe <105961163+parkjjoe@users.noreply.github.com>
2024-03-08 10:01:46 +0900

    [Gold V] Title: 별 찍기 - 10, Time: 56 ms, Memory: 40556 KB -BaekjoonHub

---
+++ 백준/Gold/2447. 별 찍기 － 10/별 찍기 － 10.py
@@ -0,0 +1,23 @@
+import sys
+
+n = int(sys.stdin.readline())
+
+def draw_stars(n):
+  if n==1:
+    return ['*']
+
+  Stars=draw_stars(n//3)
+  L=[]
+
+  for star in Stars:
+    L.append(star*3)
+    
+  for star in Stars:
+    L.append(star+' '*(n//3)+star)
+    
+  for star in Stars:
+    L.append(star*3)
+
+  return L
+
+print('\n'.join(draw_stars(n)))
