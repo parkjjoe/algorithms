c4a818a630b7004197cdd1f2e76a6789e74bafc1
parkjjoe <105961163+parkjjoe@users.noreply.github.com>
2024-03-16 21:50:26 +0900

    [Bronze II] Title: Hashing, Time: 40 ms, Memory: 31120 KB, Score: 100 point -BaekjoonHub

---
+++ 백준/Bronze/15829. Hashing/Hashing.py
@@ -0,0 +1,12 @@
+import sys
+
+L = int(sys.stdin.readline())
+string = str(sys.stdin.readline().rstrip())
+
+total = 0
+alphabet = {"a": 1, "b": 2, "c": 3, "d": 4, "e": 5, "f": 6, "g": 7, "h": 8 , "i": 9, "j": 10, "k": 11, "l": 12, "m": 13, "n": 14, "o": 15, "p": 16, "q": 17, "r": 18, "s": 19, "t": 20, "u": 21, "v": 22, "w": 23, "x": 24, "y": 25, "z": 26}
+
+for i in range(len(string)):
+	total += alphabet[string[i]] * (31 ** i)
+
+print(total % 1234567891)
