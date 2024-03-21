87b7096de7bda2f38c720e23281233985c3f901b
parkjjoe <105961163+parkjjoe@users.noreply.github.com>
2024-03-21 09:56:57 +0900

    [Silver V] Title: 방 번호, Time: 44 ms, Memory: 31120 KB -BaekjoonHub

---
+++ 백준/Silver/1475. 방 번호/방 번호.py
@@ -0,0 +1,16 @@
+import sys
+
+n = int(sys.stdin.readline())
+
+number = [0] * 10
+
+for i in str(n):
+    if i == "6" or i == "9":
+        if number[6] == number[9]:
+            number[6] += 1
+        else:
+            number[9] += 1
+    else:
+        number[int(i)] += 1
+
+print(max(number))
