0d3580cc3c192c21a5d4f41b1b2dfb215d681cc1
parkjjoe <105961163+parkjjoe@users.noreply.github.com>
2024-03-24 14:34:15 +0900

    [Bronze II] Title: Strfry, Time: 152 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Bronze/11328. Strfry/Strfry.py
+++ 백준/Bronze/11328. Strfry/Strfry.py
@@ -4,8 +4,5 @@ n = int(sys.stdin.readline())
 
 for _ in range(n):
 	a, b = map(str, sys.stdin.readline().rstrip().split())
-
-	if sorted(a) != sorted(b):
-		print("Impossible")
-	else:
-		print("Possible")
+	
+	print("Impossible") if sorted(a) != sorted(b) else print("Possible")
