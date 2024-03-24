8b25565ecf8320129475b98597bdf3a279cc280d
parkjjoe <105961163+parkjjoe@users.noreply.github.com>
2024-03-24 16:01:56 +0900

    Create @편집 거리.py

---
+++ 이것이 취업을 위한 코딩 테스트다 with 파이썬/다이나믹 프로그래밍/@편집 거리.py
@@ -0,0 +1,27 @@
+import sys
+
+str1 = str(sys.stdin.readline().rstrip())
+str2 = str(sys.stdin.readline().rstrip())
+
+# 최소 편집 거리 계산을 위한 DP
+def edit_dist(str1, str2):
+	n, m = len(str1), len(str2)
+
+	dp = [[0] * (m + 1) for _ in range(n + 1)]
+
+	for i in range(1, n + 1):
+		dp[i][0] = i
+	for j in range(1, m + 1):
+		dp[0][j] = j
+
+	# 최소 편집 거리 계산
+	for i in range(1, n + 1):
+		for j in range(1, m + 1):
+			if str1[i - 1] == str2[j - 1]: # 문자가 같다면, 왼쪽 위에 해당하는 수 그대로 대입
+				dp[i][j] = dp[i - 1][j - 1]
+			else: # 문자가 다르다면, 왼쪽(삽입), 위쪽(삭제), 왼쪽 위(교체) 중 최소 비용을 찾아 대입
+				dp[i][j] = 1 + min(dp[i][j -1], dp[i - 1][j], dp[i - 1][j - 1])
+
+	return dp[n][m]
+
+print(edit_dist(str1, str2))
